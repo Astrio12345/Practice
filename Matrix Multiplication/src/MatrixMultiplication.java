f241a5fd44f408a3ed56964cadad44c206da31ea
Ayush Kumar Srivastava <167962127+Astrio12345@users.noreply.github.com>
2025-09-03 23:55:12 +0530

    Initial commit - added Matrix Multiplication Program

---
+++ Matrix Multiplication/src/MatrixMultiplication.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows for Matrix A: ");
+        int rowsA = sc.nextInt();
+        System.out.print("Enter number of columns for Matrix A: ");
+        int colsA = sc.nextInt();
+        int[][] A = new int[rowsA][colsA];
+        System.out.println("Enter elements of Matrix A:");
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsA; j++) {
+                A[i][j] = sc.nextInt();
+            }
+        }
+        System.out.print("Enter number of rows for Matrix B: ");
+        int rowsB = sc.nextInt();
+        System.out.print("Enter number of columns for Matrix B: ");
+        int colsB = sc.nextInt();
+        int[][] B = new int[rowsB][colsB];
+        System.out.println("Enter elements of Matrix B:");
+        for (int i = 0; i < rowsB; i++) {
+            for (int j = 0; j < colsB; j++) {
+                B[i][j] = sc.nextInt();
+            }
+        }
+        if (colsA != rowsB) {
+            System.out.println("Matrix multiplication not possible! (Columns of A != Rows of B)");
+            return;
+        }
+        int[][] result = new int[rowsA][colsB];
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                for (int k = 0; k < colsA; k++) {
+                    result[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        System.out.println("Result of Matrix Multiplication:");
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
