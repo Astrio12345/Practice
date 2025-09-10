f15ab84bc71c72fffe418be724c349b0dfc1a3df
Ayush Kumar Srivastava <167962127+Astrio12345@users.noreply.github.com>
2025-09-10 11:28:15 +0530

    Initial commit - added Insertion Sort Program

---
+++ Insertion Sort/src/InsertionSort.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class InsertionSort {
+    static void insertionSort(int arr[], int n) {
+        if (n <= 1) {
+            return;
+        }
+        insertionSort(arr, n - 1);
+        int last = arr[n - 1];
+        int j = n - 2;
+        while (j >= 0 && arr[j] > last) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = last;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter elements of array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        insertionSort(arr, arr.length);
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        sc.close();
+    }
+}
