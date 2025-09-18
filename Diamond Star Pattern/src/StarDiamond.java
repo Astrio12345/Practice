67e8f9ce858388977a26a8e5d47cb28b78e6cb1d
Ayush Kumar Srivastava <167962127+Astrio12345@users.noreply.github.com>
2025-09-19 00:44:31 +0530

    Initial commit - added Diamond Star Pattern program

---
+++ Diamond Star Pattern/src/StarDiamond.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class StarDiamond {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows (half of diamond): ");
+        int rows = sc.nextInt();
+        for (int i = 1; i <= rows; i++) {
+            for (int j = i; j < rows; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = rows - 1; i >= 1; i--) {
+            for (int j = i; j < rows; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
